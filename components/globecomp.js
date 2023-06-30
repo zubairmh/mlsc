72a329325ea9ccb3a9aff2f21706776b65a6a376
zubairmh <zubair.mh@protonmail.com>
2023-07-01 03:08:05 +0530

    make globe larger on pc

--- components/globecomp.js
+++ components/globecomp.js
@@ -2,7 +2,7 @@ import Globe from "@/components/globe";
 export default function GlobeComp() {
   return (
     <div className="flex justify-center items-center flex-row min-h-screen">
-      <div className="h-96 w-96 rounded-full">
+      <div className="h-96 w-96 md:h-[35rem] md:w-[35rem] rounded-full">
         <Globe scale={1} markerArray={[]} />
       </div>
     </div>
