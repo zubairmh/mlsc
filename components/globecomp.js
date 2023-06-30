bcebc30b705463d1fc49414f43716e494e7c2d24
zubairmh <zubair.mh@protonmail.com>
2023-07-01 03:04:42 +0530

    fixed globe scaling

--- components/globecomp.js
+++ components/globecomp.js
@@ -1,6 +1,10 @@
-import Globe from "@/components/globe"
+import Globe from "@/components/globe";
 export default function GlobeComp() {
-    return <div className="flex justify-center items-center overflow-hidden min-h-screen">
-        <Globe scale={0.5} markerArray={[]}/>
+  return (
+    <div className="flex justify-center items-center flex-row min-h-screen">
+      <div className="h-96 w-96 rounded-full">
+        <Globe scale={1} markerArray={[]} />
+      </div>
     </div>
+  );
 }
