7747722b0805bb78f3968b637aa5445efd87fecc
zubairmh <zubair.mh@protonmail.com>
2023-07-01 19:45:39 +0530

    Added card animations

--- components/navbar.js
+++ components/navbar.js
@@ -1,23 +1,52 @@
 import Image from "next/image";
 import MLSC from "@/public/mlsc.png";
 import Link from "next/link";
+import { ToastAction } from "@/components/ui/toast";
+import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "@/components/ui/use-toast";
 import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
 const inter = Inter({ weight: "700", subsets: ["latin"] });
 const inter2 = Inter({ weight: "500", subsets: ["latin"] });
 import GlobeComp from "@/components/globecomp";
+import { Button } from "@/components/ui/button";
+import { GiHamburgerMenu } from "react-icons/gi";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useRouter } from "next/router";
+
 export default function Navbar({ children }) {
-    const [loading, setLoading]=useState(true);
-    useEffect(() => {
-      setLoading(true)
-      setTimeout(()=>{
-        setLoading(false)
-      }, 4100)
-    }, [])
-    if(loading) {
-      return <GlobeComp/>
-    }
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  const { toast } = useToast();
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 100);
+  }, []);
+  let paths = {
+    "/": "Home",
+    "/about": "About",
+    "/events": "Events",
+    "/team": "Team",
+    "/form": "Form",
+  };
+  if (loading) {
+    return <GlobeComp />;
+  }
   //   useEffect(() => {
   //     toast.loading("Loading", {
   //       position: toast.POSITION.BOTTOM_RIGHT,
@@ -26,21 +55,61 @@ export default function Navbar({ children }) {
   //   }, []);
   return (
     <div className="flex flex-col min-h-screen min-w-full text-white fixed">
-      <div className={`h-16 flex flex-row w-full p-5 items-center gap-4 ${inter2.className} fixed`}>
+      <div
+        className={`h-16 flex flex-row w-full p-5 items-center gap-4 ${inter2.className} fixed`}
+      >
         <Link className="flex flex-row items-center gap-4" href="/">
           <Image alt="MLSC logo" width={32} height={32} src={MLSC}></Image>
           <h1 className={inter.className}>MLSC</h1>
         </Link>
         <div className="grow"></div>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/team">Team</Link>
-        <Link href="/events">Events</Link>
-        <Link href="#">Form</Link>
+        <div className="hidden sm:flex flex-row  gap-4">
+          <Link href="/"> Home </Link>
+          <Link href="/about">About</Link>
+          <Link href="/team">Team</Link>
+          <Link href="/events">Events</Link>
+          <button
+            onClick={() => {
+              toast({
+                variant:"destructive",
+                title: "Forms are Closed",
+              });
+            }}
+          >
+            Form
+          </button>
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="block sm:hidden" variant="primary">
+              <GiHamburgerMenu />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56 bg-[#0f0f0f] text-white border-gray-800">
+            <DropdownMenuLabel>Menu</DropdownMenuLabel>
+            <DropdownMenuSeparator className="bg-gray-800" />
+            <DropdownMenuGroup>
+              <DropdownMenuItem onClick={() => router.push("/")}>
+                Home
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => router.push("/about")}>
+                About
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => router.push("/team")}>
+                Team
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => router.push("/events")}>
+                Events
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => router.push("/form")}>
+                Form
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
-      <ToastContainer />
+      <Toaster/>
       {children}
-      
     </div>
   );
 }
--- components/team2.js
+++ components/team2.js
@@ -1,12 +1,16 @@
 import Reet from "@/public/reet.jpeg"
 import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ weight: "700", subsets: ["latin"] });
 export default function TeamComponent2() {
   return (
     <div className="flex justify-center items-center grow p-14">
       <div class="gap-10 grid grid-cols-1 sm:grid-cols-4 overflow-y-auto h-[38rem] text-white grow p-3">
         {[...Array(7)].map((x, i) => (
           <div key={i} className="group w-64 h-64 relative text-center bg-slate-300 outline-none outline-offset-4 outline-4 hover:outline-[#FD3D48]">
-            <h1 className="absolute text-xl bottom-0 left-[50%] font-bold z-10 whitespace-nowrap translate-x-[-50%] translate-y-[-50%] group-hover:translate-y-[-90%] group-hover:scale-125 transition delay-75 cursor-default">Reetinder Singh</h1>
+            <h1 style={inter.style} className="absolute text-xl bottom-0 left-[50%] font-bold z-10 whitespace-nowrap translate-x-[-50%] translate-y-[-50%] group-hover:translate-y-[-110%] group-hover:scale-125 transition delay-75 cursor-default">Reetinder Singh</h1>
+            <h4 className="absolute text-md bottom-0 left-[50%] font-bold z-10 whitespace-nowrap translate-x-[-50%] translate-y-[-10%] group-hover:translate-y-[-30%] group-hover:scale-125 opacity-0 group-hover:opacity-100 transition delay-75 text-gray-200 cursor-default">General Secretary</h4>
             <div className="absolute h-full w-full group-hover:bg-gradient-to-t from-red-600 via-transparent to-transparent"/>
             <Image alt="Team member" width={500} height={500} src={Reet} className="h-full w-full" />
           </div>
