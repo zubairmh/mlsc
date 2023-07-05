eefb168a5b0027c5fab987e41a276fee13901dc1
zubairmh <zubair.mh@protonmail.com>
2023-07-05 14:28:15 +0530

    typo

--- components/home.js
+++ components/home.js
@@ -11,7 +11,7 @@ export default function Home() {
             <Image src={MLSC} alt="" width={351} height={325} className="scale-50 drop-shadow-2xl sm:scale-100 z-10"/>
             <div class="flex flex-col gap-2 text-center lg:text-left z-10">
                 <h1 class="text-5xl lg:text-7xl font-bold tracking-wide bg-gradient-to-br from-pink-500  to-purple-700 bg-clip-text text-transparent">MICROSOFT</h1>
-                <h1 class="text-4xl lg:text-6xl font-bold tracking-wide bg-gradient-to-br from-cyan-500  to-blue-700 bg-clip-text text-transparent">LEARNING STUDENT</h1>
+                <h1 class="text-4xl lg:text-6xl font-bold tracking-wide bg-gradient-to-br from-cyan-500  to-blue-700 bg-clip-text text-transparent">LEARN STUDENT</h1>
                 <h1 class="text-3xl lg:text-5xl">CHAPTER</h1>
             </div>
             <div class="absolute top-[5rem] right-[-1rem] rounded-full  w-28 h-28 bg-gradient-to-br from-pink-500  to-purple-700 animation-floating">
